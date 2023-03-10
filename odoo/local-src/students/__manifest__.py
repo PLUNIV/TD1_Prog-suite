3fa2b563636037d97d7eded9dfca4932c2c7c608
PLUNIV <pierrelouisdebuysschere@gmail.com>
2023-03-10 16:51:26 +0100

    Initial commit

---
+++ odoo/local-src/students/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name" : "Gestion des étudiants",
+    "version" : "0.8",
+    "category" : "Generic Modules/Others",
+    "description" : """Test création module gestion des étudiants Odoo v14""",
+    "author" : "Debuysschere",
+    "data": ["views/students_views.xml","views/training_views.xml"],
+    "depends" : ["base"],
+    "installable" : True,
+    "auto_install" : False,
+}
